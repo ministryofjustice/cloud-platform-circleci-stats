511700ccd13f2b27f00fabc3138b37db99caa775
David Salgado <david.salgado@digital.justice.gov.uk>
2019-06-21 16:53:14 +0100

    Display more details of each job

--- web.rb
+++ web.rb
@@ -2,18 +2,41 @@
 
 require 'bundler/setup'
 require 'sinatra'
+require 'date'
 require './lib/db'
 
 class Build
   attr_reader :number, :queued_at, :start_time, :duration, :vcs_url, :committer_email
 
   def initialize(args)
-    @number = args.fetch('number')
-    @queued_at = args.fetch('queued_at')
-    @start_time = args.fetch('start_time')
-    @duration = args.fetch('duration')
+    @number = args.fetch('number').to_i
+    @queued_at = args.fetch('queued_at').to_i
+    @start_time = args.fetch('start_time').to_i
+    @duration = args.fetch('duration').to_i
     @vcs_url = args.fetch('vcs_url')
     @committer_email = args.fetch('committer_email')
+  end
+
+  def started
+    formatted_time start_time
+  end
+
+  def queued
+    formatted_time queued_at
+  end
+
+  private
+
+  def formatted_time(t)
+    t == 0 ? '' : epoch_to_formatted(t)
+  end
+
+  def epoch_to_formatted(epoch)
+    epoch_to_datetime(epoch).strftime("%Y-%m-%d %H:%M:%S")
+  end
+
+  def epoch_to_datetime(epoch)
+    Time.at(epoch).to_datetime
   end
 end
 
@@ -48,13 +71,17 @@ __END__
 <table>
   <thead>
     <tr>
-      <th>Number</th>
+      <th>Queued</th>
+      <th>Started</th>
+      <th>Duration</th>
     </tr>
   </thead>
   <tbody>
     <% @builds.each do |build| %>
       <tr>
-        <td><%= build.number %></td>
+        <td><%= build.queued %></td>
+        <td><%= build.started %></td>
+        <td><%= build.duration %></td>
       </tr>
     <% end %>
   </tbody>
